da8946396a0de206fb542a3886abca5a8603f5a2
Vyshnavi22 <lekkaladevika@gmail.com>
2026-06-10 19:32:22 +0530

    first commit by Devika

---
+++ src/main/java/com/backendDev/BackendTableTopLeoDevApplication.java
@@ -0,0 +1,13 @@
+package com.backendDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTableTopLeoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTableTopLeoDevApplication.class, args);
+	}
+
+}
